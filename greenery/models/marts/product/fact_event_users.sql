e7618aa162f45995200f434ad006b00578d57988
kaleynguyen <nana.ngannguyen@gmail.com>
2022-10-17 06:00:49 +0000

    Add marts/product and update week2 README

---
+++ greenery/models/marts/product/fact_event_users.sql
@@ -0,0 +1,24 @@
+{{config(
+    materialized='table'
+)}}
+
+SELECT
+    user_id
+    , u.first_name
+    , u.last_name
+    , u.zipcode
+    , MIN(e.created_at_utc) last_session
+    , MAX(e.created_at_utc) first_session
+    , SUM(package_shipped) package_shipped
+    , SUM(page_view) page_view
+    , SUM(add_to_cart) add_to_cart
+    , SUM(checkout) checkout
+    , SUM(order_total) AS order_total
+    , SUM(num_orders) AS num_orders
+    , COUNT(distinct session_id) AS num_unique_session
+    , ROUND(SUM(num_orders)/COUNT(DISTINCT session_id)::NUMERIC,2) AS order_session_ratio
+    , ROUND(SUM(num_orders)/SUM(page_view)::NUMERIC,2) as order_pageview_ratio
+FROM {{ref("int_event_types")}} e
+LEFT JOIN {{ref('dim_users')}} u USING(user_id)
+LEFT JOIN {{ref("fact_orders")}} o USING(user_id)
+GROUP BY 1,2,3,4
---
+++ greenery/models/marts/product/int_event_types.sql
@@ -0,0 +1,14 @@
+{{config(
+    materialized='table')
+}}
+
+
+
+SELECT 
+    user_id
+    , session_id
+    , created_at_utc
+    , {{ dbt_utils.pivot('event_type', dbt_utils.get_column_values(ref('stg_events'), 'event_type')) }}
+    , COUNT(distinct order_id) as num_orders
+FROM {{ref("stg_events")}}
+GROUP BY 1,2,3
